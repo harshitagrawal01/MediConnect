9adeabd760b0bb1685329f3a487e2e959a46c41d
Harshit Agrawal <harshit0114ag@gmail.com>
2026-02-27 21:02:40 +0530

    correction in socket

--- fronted/src/socket.js
+++ fronted/src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
-  autoConnect: true,      
-  transports: ["websocket"], 
+  autoConnect: false,      
+  transports: ["websocket", "polling"], 
 });
 
 export default socket;

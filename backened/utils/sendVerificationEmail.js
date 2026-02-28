87db8496ed993f0f728647b9ea8ef9e4a26e93a4
Harshit Agrawal <harshit0114ag@gmail.com>
2026-02-28 15:29:25 +0530

    fix: use port 465 for SMTP on Render

--- backened/utils/sendVerificationEmail.js
+++ backened/utils/sendVerificationEmail.js
@@ -13,8 +13,8 @@ const sendVerificationEmail = async (user) => {
 
     const transporter = nodemailer.createTransport({
       host: 'smtp-relay.brevo.com',
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.BREVO_EMAIL,
         pass: process.env.BREVO_SMTP_KEY

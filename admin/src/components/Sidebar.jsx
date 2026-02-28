070a826b12c82a1a752db1323bbd516b6aab0a9e
Harshit Agrawal <harshit0114ag@gmail.com>
2026-03-01 02:37:34 +0530

    mobile responsiveness improve

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -15,22 +15,22 @@ const Sidebar = () => {
         aToken && <ul className='text-[#515151] mt-5'>
 
           <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''}`} to={'/admin-dashboard'} >
-            <img src={assets.home_icon} alt="" />
+            <img className='w-5 min-w-5' src={assets.home_icon} alt="" />
             <p className='hidden md:block'>Dashboard</p>
           </NavLink>
 
           <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''}`} to={'/all-appointments'} >
-            <img src={assets.appointment_icon} alt="" />
+            <img className='w-5 min-w-5'  src={assets.appointment_icon} alt="" />
             <p className='hidden md:block'>Appointments</p>
           </NavLink>
 
           <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50  border-r-4 border-primary' : ''}`} to={'/add-doctor'} >
-            <img src={assets.add_icon} alt="" />
+            <img className='w-5 min-w-5'  src={assets.add_icon} alt="" />
             <p className='hidden md:block'>Add Doctor</p>
           </NavLink>
 
           <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50  border-r-4 border-primary' : ''}`} to={'/doctor-list'} >
-            <img src={assets.people_icon} alt="" />
+            <img className='w-5 min-w-5'  src={assets.people_icon} alt="" />
             <p className='hidden md:block'>Doctors List</p>
           </NavLink>
 
@@ -41,17 +41,17 @@ const Sidebar = () => {
         dToken && <ul className='text-[#515151] mt-5'>
 
           <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''}`} to={'/doctor-dashboard'} >
-            <img src={assets.home_icon} alt="" />
+            <img className='w-5 min-w-5'  src={assets.home_icon} alt="" />
             <p className='hidden md:block'>Dashboard</p>
           </NavLink>
 
           <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''}`} to={'/doctor-appointments'} >
-            <img src={assets.appointment_icon} alt="" />
+            <img className='w-5 min-w-5'  src={assets.appointment_icon} alt="" />
             <p className='hidden md:block'>Appointments</p>
           </NavLink>
 
           <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''}`} to={'/doctor-profile'} >
-            <img src={assets.people_icon} alt="" />
+            <img className='w-5 min-w-5'  src={assets.people_icon} alt="" />
             <p className='hidden md:block'>Profile</p>
           </NavLink>
 

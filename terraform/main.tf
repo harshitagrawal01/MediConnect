556093d6ea9455b1cfaebbc7f62754fc433ea9af
Harshit Agrawal <harshit0114ag@gmail.com>
2026-08-30 14:01:38 +0530

    Added subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,49 @@ resource "aws_vpc" "mediconnect_vpc" {
     Name = "mediconnect-vpc"
   }
 }
+
+# Public Subnet 1 - Availability Zone A
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.mediconnect_vpc.id
+  cidr_block               = "10.0.1.0/24"
+  availability_zone        = "us-east-1a"
+  map_public_ip_on_launch  = true
+
+  tags = {
+    Name = "mediconnect-public-subnet-1"
+  }
+}
+
+# Public Subnet 2 - Availability Zone B
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.mediconnect_vpc.id
+  cidr_block               = "10.0.2.0/24"
+  availability_zone        = "us-east-1b"
+  map_public_ip_on_launch  = true
+
+  tags = {
+    Name = "mediconnect-public-subnet-2"
+  }
+}
+
+# Private Subnet 1 - Availability Zone A
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.mediconnect_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "mediconnect-private-subnet-1"
+  }
+}
+
+# Private Subnet 2 - Availability Zone B
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.mediconnect_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "mediconnect-private-subnet-2"
+  }
+}

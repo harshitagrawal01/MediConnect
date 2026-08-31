a120108e5744488e9be493e80143f2c2992c8092
Harshit Agrawal <harshit0114ag@gmail.com>
2026-08-31 13:45:13 +0530

    added resource for eks cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,3 +40,27 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
 }
+
+resource "aws_eks_cluster" "mediconnect_cluster" {
+  name     = "mediconnect-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public_subnet_1.id,
+      aws_subnet.public_subnet_2.id,
+      aws_subnet.private_subnet_1.id,
+      aws_subnet.private_subnet_2.id
+    ]
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name = "mediconnect-cluster"
+  }
+}

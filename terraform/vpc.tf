e009fbd1e9f3199b4484336bfc5a099ad09cf14f
Harshit Agrawal <harshit0114ag@gmail.com>
2026-09-01 01:35:29 +0530

    added nat gateway

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -88,3 +88,50 @@ resource "aws_route_table_association" "public_2_assoc" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "mediconnect-nat-eip"
+  }
+}
+
+# NAT Gateway - lives in a PUBLIC subnet
+resource "aws_nat_gateway" "mediconnect_nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "mediconnect-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.mediconnect_igw]
+}
+
+# Private Route Table - routes through NAT Gateway
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.mediconnect_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.mediconnect_nat.id
+  }
+
+  tags = {
+    Name = "mediconnect-private-rt"
+  }
+}
+
+# Associate Private Subnet 1 with Private Route Table
+resource "aws_route_table_association" "private_1_assoc" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# Associate Private Subnet 2 with Private Route Table
+resource "aws_route_table_association" "private_2_assoc" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}

6d86308f8321f7341beb9c1b60da58037b1cc8e7
Harshit Agrawal <harshit0114ag@gmail.com>
2026-09-08 00:44:07 +0530

    added capacity of pod

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,8 +112,8 @@ resource "aws_eks_node_group" "mediconnect_nodes" {
   ]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 
